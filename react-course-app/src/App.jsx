5b34a72922a72307633901223596ca605ee1105c
Andres Merida <andresmerida1@gmail.com>
2026-03-07 23:59:15 -0400

    Adding react-rauter

--- react-course-app/src/App.jsx
+++ react-course-app/src/App.jsx
@@ -1,13 +1,29 @@
 import './App.css'
-import {useState} from "react";
-import SignUpForm from "./components/SignUpForm.jsx";
+import { Route, Routes, Link } from "react-router-dom";
+
+function HomePage() {
+  return <h1>Home Page</h1>
+}
+
+function AboutPage() {
+  return <h1>About Page</h1>
+}
 
 function App() {
 
   return (
-    <>
-      <SignUpForm/>
-    </>
+    <div>
+      <nav style={{ display: 'flex', gap: '1rem', marginBottom: '1rem'}}>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<h1>404 Page not found</h1>} />
+      </Routes>
+      <footer>Footer</footer>
+    </div>
   )
 }
 
