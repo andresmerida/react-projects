e25bb6edcdfbe8df79a75c3266ddf45ed349c108
Andres Merida <andresmerida1@gmail.com>
2026-03-07 23:22:12 -0400

    Adding react-hook-form

--- react-course-app/src/App.jsx
+++ react-course-app/src/App.jsx
@@ -1,20 +1,12 @@
 import './App.css'
 import {useState} from "react";
-
-function Greeting(props) {
-  return <h1>Hello, I am Andres {props.name}</h1>
-}
+import SignUpForm from "./components/SignUpForm.jsx";
 
 function App() {
-  const [showGreeting, setShowGreeting] = useState(false)
 
-  function toggleGreeting() {
-    setShowGreeting(prevState => !prevState)
-  }
   return (
     <>
-      <button onClick={toggleGreeting}>Toggle Greeting</button>
-      {showGreeting && <Greeting name={"Andres"}/>}
+      <SignUpForm/>
     </>
   )
 }
