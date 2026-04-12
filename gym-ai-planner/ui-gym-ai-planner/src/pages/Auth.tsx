6365e0351486cf1ef1a2ee2075afa9ba157a1ca3
Andres Merida <andresmerida1@gmail.com>
2026-04-11 23:50:24 -0400

    initial commit

---
+++ gym-ai-planner/ui-gym-ai-planner/src/pages/Auth.tsx
@@ -0,0 +1,13 @@
+import { AuthView } from "@neondatabase/neon-js/auth/react";
+import {useParams} from "react-router-dom";
+
+export default function Auth() {
+  const { pathname } = useParams();
+  return (
+    <div className={"flex items-center justify-center min-h-screen pt-24 pb-12 px-6"}>
+      <div className={"max-w-md w-full"}>
+        <AuthView pathname={pathname}/>
+      </div>
+    </div>
+  )
+}
---
+++ gym-ai-planner/ui-gym-ai-planner/src/pages/Profile.tsx
@@ -0,0 +1,17 @@
+import {useAuth} from "../context/AuthContext.tsx";
+import {Navigate} from "react-router-dom";
+
+export default function Profile() {
+  const {user, isLoading} = useAuth();
+  const plan = false;
+
+  if (!user && !isLoading) {
+    return <Navigate to={"/auth/sign-in"} replace />
+  }
+
+  if (!plan) {
+    return <Navigate to={"/onboarding"} replace />
+  }
+
+  return <h1>Profile Page</h1>
+}
